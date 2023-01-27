2b2cbf024cde4151f6e5667e3f7dbb32daab71c3
Catalin Narita <catalin.narita@avios.com>
2023-01-27 12:44:02 +0200

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Vezi ca te musca cainii drace</p>
+        <p>asdf</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
